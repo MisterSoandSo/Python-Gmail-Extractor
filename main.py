60d03e2ffc7e6cf98c6ea55916687ca595c0313e
Andrew So <asoso221@gmail.com>
2023-07-03 11:57:53 -0700

    Initial Commit
    Setup basic terminal menu
    Connect to GMail API and retrieve credentials

---
+++ main.py
@@ -0,0 +1,50 @@
+from dotenv import load_dotenv
+from backend.backend import credentials
+
+import os
+
+#Configurations
+load_dotenv('.env')
+token = os.environ.get("token_path")
+secret = os.environ.get("credential_path")
+SCOPES = [os.environ.get("scope")]
+
+# GMail API credential generation
+# Function will take you Oauth and select the email address yo uwish to use. This will only for the first run.
+creds = credentials(secret,token,SCOPES)    
+
+def main_menu():
+    print("~~~~~ Python GMail Terminal Menu ~~~~~")
+    print("1. Option 1")
+    print("2. Option 2")
+    print("3. Option 3")
+    print("4. Option 4")
+    print("5. Exit")
+
+def option1():
+    pass
+def option2():
+    pass
+def option3():
+    pass
+def option4():
+    pass
+
+def main():
+    menu_options = {
+        '1': option1,
+        '2': option2,
+        '3': option3,
+        '4': option4,
+        '5': exit
+    }
+    while True:
+        main_menu()
+        choice = input("Select >> ")
+        if choice in menu_options:
+            menu_options[choice]()
+        else:
+            print("Invalid Input")
+
+if __name__ == '__main__':
+    main()
