e4f7ad0b5311ca59cc257d0923c023123c7ac617
Andrew So <asoso221@gmail.com>
2023-07-08 16:27:27 -0700

    Update scraper.py
    Add custom BeautifulSoup class to extract tables, images, links, and texts

--- backend/scraper.py
+++ backend/scraper.py
@@ -1,3 +1,4 @@
+from bs4 import BeautifulSoup
 import re
 
 class EmailReader():
@@ -20,7 +21,7 @@ class EmailReader():
         if html_section:
             # Extracted HTML portion
             self.html_content = html_section.group()
-            print(self.html_content)
+            #print(self.html_content)
         else:
             print("HTML portion not found in the text file.")
 
@@ -30,3 +31,44 @@ class EmailReader():
         html_start_index = html_section.start()
         self.nonhtml_content = self.content[:html_start_index]
 
+
+class SoupStrainer():
+    def __init__(self,content = None,filename = None) -> None:
+        if content:
+            html_content = EmailReader().passContent(content)
+        if filename:
+            html_content = EmailReader().readFile(filename)
+        self.soup = BeautifulSoup(html_content, 'html.parser')    
+        
+    #Extract Tags
+    def find_table(self):
+        tables = self.soup.find_all("table")
+        tab_list = []
+        for table in tables:
+            row_list = []
+            for row in table.find_all("tr"):
+                col_list = []
+                for cell in row.find_all("td"):
+                    col_list.append(cell.text)
+                row_list.append(col_list)
+            tab_list.append(row_list)
+        return tab_list
+    
+    def find_atags(self):
+        a_tags = self.soup.find_all('a')
+        for a in a_tags:
+            print("Text:", a.text)
+            print("Href:", a.get('href'))
+            print() 
+
+    def find_ptags(self):
+        p_tags = self.soup.find_all('p')
+        for p in p_tags:
+            print(p.text)
+            print() 
+
+    def find_imgs(self):
+        images = self.soup.find_all("img")
+        for image in images:
+            image_url = image["src"]
+            print(image_url)
