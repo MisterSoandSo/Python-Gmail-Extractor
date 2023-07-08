67e29b0f88f44e38297c7f0e4721a40ae54ed2a2
Andrew So <asoso221@gmail.com>
2023-07-07 17:52:38 -0700

    Implement attachments download function

--- backend/backend.py
+++ backend/backend.py
@@ -25,13 +25,6 @@ def credentials(credential, token, SCOPES):
             token.write(creds.to_json())
     
     return creds
-
-
-def write_email_to_text_file(email_id, body_content):
-    file_path = "emails/"+ str(email_id)+ ".txt"  # Specify the file path
-    with open(file_path, "w", encoding="utf-8") as file:
-        # Write the string to the file
-        file.write(body_content)
         
 
 #Handle Extracting information for specifc email ids and marking them as read.
@@ -57,24 +50,57 @@ class EmailMessage():
         return [header['value'] for header in headers if header['name'] == 'Date'][0]
     
     def get_Body(self):
-        parts = self.message['payload']['parts']
-        # Recursive function to decode parts
-        def decode_parts(parts):
-            content = ""
-            for part in parts:
-                if 'parts' in part:
-                    # If part has nested parts, recursively decode them
-                    content += decode_parts(part['parts'])
-                if 'body' in part and 'data' in part['body']:
-                    # Decode the part's data and append to content
-                    data = part['body']['data']
-                    decoded_data = base64.urlsafe_b64decode(data).decode()
-                    content += decoded_data
-            return content
-        return decode_parts(parts)
-    
+        try:
+            parts = self.message['payload']['parts']
+            # Recursive function to decode parts
+            def decode_parts(parts):
+                content = ""
+                for part in parts:
+                    if 'parts' in part:
+                        # If part has nested parts, recursively decode them
+                        content += decode_parts(part['parts'])
+                    if 'body' in part and 'data' in part['body']:
+                        # Decode the part's data and append to content
+                        data = part['body']['data']
+                        decoded_data = base64.urlsafe_b64decode(data).decode()
+                        content += decoded_data
+                return content
+            return decode_parts(parts)
+        except:
+            return "Payload formating Error"
+        
     def mark_Read(self) -> None:
         self.service.users().messages().modify(userId='me', id=self.email_id, body={'removeLabelIds': ['UNREAD']}).execute()
+
+    def get_attachments(self):   
+        try:
+            parts = self.message['payload']['parts']
+            # Check if the email has attachments     
+        
+            for part in parts:
+                file_name = part['filename']
+                body = part['body']
+                
+                if 'attachmentId' in body:
+                    attachment_id = body['attachmentId']
+                    response = self.service.users().messages().attachments().get(userId='me',
+                                                                            messageId=self.email_id,
+                                                                            id=attachment_id
+                                                                            ).execute()
+                    file_data = base64.urlsafe_b64decode(response.get('data').encode('UTF-8'))
+                    file_path = "emails/"+str(self.email_id)
+                    os.makedirs(file_path)
+                    with open(file_path+"/"+file_name, 'wb') as _f:
+                        _f.write(file_data)
+                        print(f'File {file_name} is saved.')
+        except:
+            pass
+
+    def write_email_to_text_file(self, body_content):
+        file_path = "emails/"+ str(self.email_id)+ ".txt"  # Specify the file path
+        with open(file_path, "w", encoding="utf-8") as file:
+            # Write the string to the file
+            file.write(body_content)
 
 
 # Class handles parsing all Email Ids from email
@@ -93,13 +119,13 @@ class MailList():
             exit()
         else:
             for message in messages:
-                try:
-                    print("Message ID: ",message['id'])
-                    msg = EmailMessage(self.creds,message['id'])
-                    print("Subject: ",msg.get_Subject())
-                    print("From: ", msg.get_Sender())
-                    print("-"*45)
-                except:
-                    print("Error ...")
+                print("Message ID: ",message['id'])
+                msg = EmailMessage(self.creds,message['id'])
+                print("Subject: ",msg.get_Subject())
+                print("From: ", msg.get_Sender())
+                msg.write_email_to_text_file(msg.get_Body())
+                msg.get_attachments()
+                print("-"*45)
+                
         
     
